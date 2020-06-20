65f102d35c746325fb5804be46cb090018dd8b69
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-20 14:30:00 +0200

    Projection operatos and the SelectMany operator with examples

---
+++ ProjectionOperators/ProjectionOperators/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectionOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Employee Id's
+            IEnumerable<int> employeeIds = Employee.GetAllEmployees().Select(emp => emp.EmployeeID);
+            foreach (var id in employeeIds)
+            {
+                Console.WriteLine(id);
+            }
+
+            // Firstname and gender
+            var genderAndFirstName = Employee.GetAllEmployees().Select(emp => new { FirstName = emp.FirstName, Gender = emp.Gender });
+            foreach(var person in genderAndFirstName)
+            {
+                Console.WriteLine("Name: " + person.FirstName + ", Gender: " + person.Gender);
+            }
+
+            // Salaries
+            var monthlySalaries = Employee.GetAllEmployees()
+                .Select(emp => new { FullName = emp.FirstName + " " + emp.LastName, MonthlySalary = emp.AnnualSalary / 12 });
+
+            foreach (var data in monthlySalaries)
+            {
+                Console.WriteLine("Name: " + data.FullName + ", monthly salary: " + data.MonthlySalary);
+            }
+
+            // Bonus
+            var entitledEmployees = Employee.GetAllEmployees()
+                .Where(emp => emp.AnnualSalary > 50000)
+                .Select(emp => new { emp.FirstName, emp.AnnualSalary, Bonus = emp.AnnualSalary * 0.1, });
+
+            foreach (var data in entitledEmployees)
+            {
+                Console.WriteLine(data.FirstName + " : " + data.AnnualSalary + " - " + data.Bonus);
+            }
+
+        }
+    }
+}
---
+++ SelectManyOperator/SelectManyOperator/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelectManyOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> listOfAllSubject = Student.GetAllStudents().SelectMany(student => student.Subjects);
+            foreach (string subject in listOfAllSubject)
+            {
+                Console.WriteLine(subject);
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine("Distinct: ");
+
+            // If you don't want to duplicate the subjects then there is distinct
+            IEnumerable<string> listOfAllDistinctSubjects = Student.GetAllStudents()
+                .SelectMany(student => student.Subjects)
+                .Distinct();
+            foreach (string subject in listOfAllDistinctSubjects)
+            {
+                Console.WriteLine(subject);
+            }
+
+            // Disctinct with sequel like syntay:
+            IEnumerable<string> listOfAllDisctinctSubject2 = from student in Student.GetAllStudents()
+                                                             from subject in student.Subjects
+                                                             select subject;
+
+            Console.WriteLine("------------");
+            // The same with sequel like syntax
+            IEnumerable<string> listOfAllSubject2 = (from student in Student.GetAllStudents()
+                                                    from subject in student.Subjects
+                                                    select subject).Distinct();
+            foreach (string subject in listOfAllSubject2)
+            {
+                Console.WriteLine(subject);
+            }
+
+            Console.WriteLine("------------");
+            // Print all the Student name's and also the subjects
+            var studentsSubjects = Student.GetAllStudents()
+                .SelectMany(student => student.Subjects, (student, subject) =>
+                new { student.Name, Subject = subject });
+
+            foreach (var student in studentsSubjects)
+            {
+                Console.WriteLine(student.Name + " has the " + student.Subject + " subject.");
+            }
+
+            Console.WriteLine("------------");
+            // Code down the above one with sequel like syntax
+            var studentsSubjects2 = from student in Student.GetAllStudents()
+                                  from subject in student.Subjects
+                                  select new { student.Name, Subject = subject };
+
+            foreach (var student in studentsSubjects2)
+            {
+                Console.WriteLine(student.Name + " has the " + student.Subject + " subject.");
+            }
+
+            string[] stringArray =
+            {
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "0123456789"
+            };
+
+            // Prining out all the characters
+            IEnumerable<char> allCharacters = stringArray
+                .SelectMany(str => str);
+
+            foreach (char ch in allCharacters)
+            {
+                Console.WriteLine(ch);
+            }
+
+            Console.WriteLine("------------");
+
+            // Coding down the one above using sequel like syntax
+            IEnumerable<char> allCharacters2 = from str in stringArray
+                                               from ch in str
+                                               select ch;
+
+            foreach (char ch in allCharacters2)
+            {
+                Console.WriteLine(ch);
+            }
+        }
+    }
+}
