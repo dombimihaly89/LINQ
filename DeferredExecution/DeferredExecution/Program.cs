5b40d398aa189b2caa4961dff0b64985b18b14ac
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-20 21:37:10 +0200

    Made examples to deferred and immediate executions

---
+++ DeferredExecution/DeferredExecution/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeferredExecution
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public int TotalMarks { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> listStudents = new List<Student>
+            {
+                new Student() { StudentID= 101, Name = "Tom", TotalMarks = 800 },
+                new Student { StudentID= 102, Name = "Mary", TotalMarks = 900 },
+                new Student { StudentID= 103, Name = "Pam", TotalMarks = 800 }
+            };
+
+            IEnumerable<Student> result = from student in listStudents
+                                          where student.TotalMarks == 800
+                                          select student;
+
+            listStudents.Add(new Student() { StudentID = 104, Name = "Dina", TotalMarks = 800 });
+
+            // The query is only executed in the foreach. It's proved because we added a new Student
+            // into the list after we wrote our query and it is also the part of the result.
+
+            foreach( Student s in result)
+            {
+                Console.WriteLine(s.StudentID + "\t" + s.Name + "\t" + s.TotalMarks);
+            }
+
+            Console.WriteLine("-------------");
+            // The below example gives us immediate execution.
+            IEnumerable<Student> result2 = (from student in listStudents
+                                          where student.TotalMarks == 800
+                                          select student).ToList();
+
+            listStudents.Add(new Student() { StudentID = 104, Name = "Maki", TotalMarks = 800 });
+            foreach (Student s in result2)
+            {
+                Console.WriteLine(s.StudentID + "\t" + s.Name + "\t" + s.TotalMarks);
+            }
+
+        }
+    }
+}
