04c0fb78156f5008a6dc21f34696f737e8d1279f
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-20 16:18:32 +0200

    ThenBy, ThenByDescending and Reverse methods' examples

---
+++ OrderingOperatorsII/OrderingOperatorsII/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderingOperatorsII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Student> studentList = Student.GetAllStudents();
+
+            // Sort students by totalmarks then by name then by id descending.
+            IOrderedEnumerable<Student> sortedStudents = studentList.OrderBy(student => student.TotalMarks)
+                .ThenBy(student => student.Name)
+                .ThenByDescending(student => student.StudentID);
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine(student.TotalMarks + " " + student.Name + " " + student.StudentID);
+            }
+
+            Console.WriteLine("---------------");
+            // Sort students by totalmarks then by name then by id descending with sequel like syntax
+            IOrderedEnumerable<Student> sortedStudents2 = from student in studentList
+                                                          orderby student.TotalMarks
+                                                          orderby student.TotalMarks, student.Name ascending, student.StudentID descending
+                                                          select student;
+            foreach (var student in sortedStudents2)
+            {
+                Console.WriteLine(student.TotalMarks + " " + student.Name + " " + student.StudentID);
+            }
+
+            Console.WriteLine("---------------");
+            // Reverse
+            Console.WriteLine("Before using reverse method");
+            foreach (var student in studentList)
+            {
+                Console.WriteLine(student.StudentID + "\t" + student.Name + "\t" + student.TotalMarks);
+            }
+            Console.WriteLine("After using reverse method");
+            IEnumerable<Student> reversedStudentList = studentList.Reverse();
+            foreach (var student in reversedStudentList)
+            {
+                Console.WriteLine(student.StudentID + "\t" + student.Name + "\t" + student.TotalMarks);
+            }
+        }
+    }
+}
---
+++ OrderingOperatorsII/OrderingOperatorsII/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderingOperatorsII
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public int TotalMarks { get; set; }
+
+        public static List<Student> GetAllStudents()
+        {
+            List<Student> listStudents = new List<Student>
+        {
+            new Student
+            {
+                StudentID= 101,
+                Name = "Tom",
+                TotalMarks = 800
+            },
+            new Student
+            {
+                StudentID= 102,
+                Name = "Mary",
+                TotalMarks = 900
+            },
+            new Student
+            {
+                StudentID= 103,
+                Name = "Pam",
+                TotalMarks = 800
+            },
+            new Student
+            {
+                StudentID= 104,
+                Name = "John",
+                TotalMarks = 800
+            },
+            new Student
+            {
+                StudentID= 105,
+                Name = "John",
+                TotalMarks = 800
+            },
+        };
+
+            return listStudents;
+        }
+    }
+}
