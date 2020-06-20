7e4ee5887e2135481ff99070f4f44cc82b7036de
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-20 14:49:42 +0200

    Sample app with differences of Select and SelectMany

---
+++ SelectAndSelectManyDifferences/SelectAndSelectManyDifferences/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelectAndSelectManyDifferences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<List<string>> result = Student.GetAllStudents().Select(student => student.Subjects);
+            foreach(List<string> subjects in result)
+            {
+                foreach(string subject in subjects)
+                {
+                    Console.WriteLine(subject);
+                }
+            }
+
+            IEnumerable<string> result2 = Student.GetAllStudents().SelectMany(student => student.Subjects);
+            foreach (string subject in result2)
+            {
+                Console.WriteLine(subject);
+            }
+        }
+    }
+}
