c8f7ebf86b822e775c12e954faa8f1eb69e5fa07
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-21 19:13:53 +0200

    Created Inner join examples

---
+++ InnerJoin/InnerJoin/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace InnerJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Employee.GetAllEmployees().Join(Department.GetAllDepartments(), emp => emp.DepartmentID, dept => dept.ID,
+                (employee, department) => new
+                {
+                    EmployeeName = employee.Name,
+                    DepartmentName = department.Name
+                });
+
+            foreach (var person in result)
+            {
+                Console.WriteLine(person.EmployeeName + " belongs to " + person.DepartmentName + " department");
+            }
+
+            Console.WriteLine("----------------------");
+            // Now with sequel like syntax
+
+            var result2 = from emp in Employee.GetAllEmployees()
+                          join dept in Department.GetAllDepartments()
+                          on emp.DepartmentID equals dept.ID
+                          select new
+                          {
+                              EmployeeName = emp.Name,
+                              DepartmentName = dept.Name
+                          };
+
+            foreach (var person in result2)
+            {
+                Console.WriteLine(person.EmployeeName + " belongs to " + person.DepartmentName + " department");
+            }
+
+        }
+    }
+}
